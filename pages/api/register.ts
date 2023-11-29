dba6426a68e93197e0f512e5f51edc623641ad95
MahdyZ7 <amahdy9@gmail.com>
2023-11-29 05:34:54 +0000

    use of database instead of json file

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -1,80 +1,55 @@
 // pages/api/register.ts
 
 import { NextApiRequest, NextApiResponse } from 'next';
-import fs from 'fs';
-import path from 'path';
+import  Database  from '@replit/database';
 
-type User = {
-  name: string;
-  id: string;
-};
+const db = new Database();
 
-const dataFilePath = path.join(process.cwd(), 'data', 'users.json');
-
-const getUsers = (): User[] => {
-  const dir = path.join(process.cwd(), 'data');
-
+const getUsers = async (): Promise<User[]> => {
   try {
-	// Ensure the directory exists, create it if it doesn't
-	if (!fs.existsSync(dir)) {
-	  fs.mkdirSync(dir, { recursive: true });
-	}
-
-	const jsonData = fs.readFileSync(dataFilePath, 'utf8');
-	return jsonData ? JSON.parse(jsonData) : [];
+    const users: User[] = await db.get('users') || [];
+    return users;
   } catch (error) {
-	// Handle the case where the file does not exist after verifying the directory does exist
-	  //	if (error instanceof Error && error as NodeJS.ErrnoException).code === 'ENOENT') {
-
-	if (error instanceof Error) {
-	  fs.writeFileSync(dataFilePath, JSON.stringify([]), 'utf8');
-	  return [];
-	} else {
-	  throw error;
-	}
+    throw error;
   }
 };
 
-export const resetUsers = () => {
-  fs.writeFileSync(dataFilePath, JSON.stringify([]), 'utf8');
-};
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
-	const { name, id } = req.body as User;
+    const { name, id } = req.body as User;
 
-	// Retrieve the current list of users
-	const users = getUsers();
+    // Retrieve the current list of users
+    const users: User[] = await db.get('users') || [];
 
-	// Check for a user with the same ID
-	const idExists = users.some(user => user.id === id);
+    // Check for a user with the same ID
+    const idExists = users.some((user) => user.id === id);
 
-	if (idExists) {
-	  // ID already in use, send a conflict response
-	  res.status(409).json({ message: `A user with the ID ${id} already exists.` });
-	  return;
-	}
+    if (idExists) {
+      // ID already in use, send a conflict response
+      res.status(409).json({ message: `A user with the ID ${id} already exists.` });
+      return;
+    }
 
-	// ID is unique, add new user to the list
-	users.push({ name, id });
-	fs.writeFileSync(dataFilePath, JSON.stringify(users), 'utf8');
-	res.status(200).json({ name, id });
+    // ID is unique, add new user to the list
+    users.push({ name, id });
+    await db.set('users', users);
+    res.status(200).json({ name, id });
   } else if (req.method === 'DELETE') {
     const secretHeader = req.headers['x-secret-header'];
-	const mySecret = process.env['resetuser']
+    const mySecret = process.env['resetuser'];
     if (!secretHeader || secretHeader !== mySecret) {
       res.status(401).json({ message: 'Unauthorized' });
       return;
     }
     try {
-      resetUsers();
+      await db.set('users', []);
       res.status(200).json({ message: 'User list has been reset.' });
     } catch (error) {
       res.status(500).json({ message: 'Internal Server Error' });
     }
   } else {
-	// Method not allowed
-	res.status(405).end();
+    // Method not allowed
+    res.status(405).end();
   }
 }
 
--- pages/api/users.ts
+++ pages/api/users.ts
@@ -1,64 +1,58 @@
 // pages/api/users.ts
 
 import { NextApiRequest, NextApiResponse } from 'next';
-import fs from 'fs';
-import path from 'path';
+import Database from '@replit/database';
 
-// Define the path to the users file
-const dataFilePath = path.join(process.cwd(), 'data', 'users.json');
+const db = new Database();
 
-// Helper function to read users from the file
-const readUsersFromFile = () => {
+// Helper function to read users from the Replit Database
+const readUsersFromDatabase = async (): Promise<any[]> => {
   try {
-	const data = fs.readFileSync(dataFilePath, 'utf8');
-  return data ? JSON.parse(data) : [];
-} catch (error) {
-  // If the file doesn't exist or is empty, return an empty array
-  if (error instanceof SyntaxError) {
-    return [];
+    const users: any[] = await db.get('users') || [];
+    return users;
+  } catch (error) {
+    throw error;
   }
-  throw error;
-}
 };
 
-// Helper function to write users to the file
-const writeUsersToFile = (users: any[]) => {
-  fs.writeFileSync(dataFilePath, JSON.stringify(users, null, 2), 'utf8');
+// Helper function to write users to the Replit Database
+const writeUsersToDatabase = async (users: any[]) => {
+  await db.set('users', users);
 };
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'GET') {
-	try {
-	  const users = readUsersFromFile();
-	  res.status(200).json(users);
-	} catch (error) {
-	  res.status(500).json({ error: 'An unexpected error occurred.' });
-	}
+    try {
+      const users = await readUsersFromDatabase();
+      res.status(200).json(users);
+    } catch (error) {
+      res.status(500).json({ error: 'An unexpected error occurred.' });
+    }
   } else if (req.method === 'POST') {
-	try {
-	  const { name, id } = req.body;
-	  if (!name || !id) {
-		res.status(400).json({ error: 'Name and UID are required.' });
-		return;
-	  }
+    try {
+      const { name, id } = req.body;
+      if (!name || !id) {
+        res.status(400).json({ error: 'Name and UID are required.' });
+        return;
+      }
 
-	  const users = readUsersFromFile();
+      const users = await readUsersFromDatabase();
 
-	  if (users.some((user: any) => user.id === id)) {
-		res.status(409).json({ error: `User with UID "${id}" already exists.` });
-		return;
-	  }
+      if (users.some((user: any) => user.id === id)) {
+        res.status(409).json({ error: `User with UID "${id}" already exists.` });
+        return;
+      }
 
-	  const newUser = { name, id };
-	  users.push(newUser);
-	  writeUsersToFile(users);
+      const newUser = { name, id };
+      users.push(newUser);
+      await writeUsersToDatabase(users);
 
-	  res.status(201).json(newUser);
-	} catch (error) {
-	  res.status(500).json({ error: 'An unexpected error occurred.' });
-	}
+      res.status(201).json(newUser);
+    } catch (error) {
+      res.status(500).json({ error: 'An unexpected error occurred.' });
+    }
   } else {
-	res.setHeader('Allow', ['GET', 'POST']);
-	res.status(405).end(`Method ${req.method} Not Allowed`);
+    res.setHeader('Allow', ['GET', 'POST']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
