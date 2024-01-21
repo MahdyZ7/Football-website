7606d2090dda34a67c5c341c2d77b757f0f9c777
MahdyZ7 <amahdy9@gamil.com>
2024-01-21 14:33:47 +0400

    footer updated to work with vercel logo

--- pages/footer.tsx
+++ pages/footer.tsx
@@ -1,20 +1,27 @@
 import React from "react";
 import Image from "next/image";
+import { useEffect } from "react";
+
+function footerText() {
+	useEffect(() => {
+		if (typeof window !== "undefined") {
+			let footer = document.getElementById("footer");
+			if (footer != null) {
+				if (window.location.href.includes("vercel")) {
+					footer.innerHTML =
+						"Running on &nbsp <span class='logo'><img src='/Vercel.svg' alt='Vercel Logo' width='60' /></span>";
+				} else {
+					footer.innerHTML =
+						"Built on <span class='logo'><img src='/replit.svg' alt='Replit Logo' width='20' height='18' /></span>Replit";
+				}
+			}
+		}
+	}, []);
+	{
+		return <div className="footer1" id="footer"></div>;
+	}
+}
 
 export default function footer() {
-  return (
-    <div className="footer1">
-      {/* <a
-		  href="/__repl"
-		  target="_blank"
-		  rel="noopener noreferrer"
-		> */}
-      Built on
-      <span className="logo">
-        <Image src="/replit.svg" alt="Replit Logo" width={20} height={18} />
-      </span>
-      Replit
-      <div style={{ height: "0rem" }} />
-    </div>
-  );
+	return footerText();
 }
