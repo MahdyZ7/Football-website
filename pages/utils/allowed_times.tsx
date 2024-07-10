b8081c3e8ab582926a5b2aba801fc8bf755d36c0
Ahmed Mahdy <amahdy9@gmail.com>
2024-07-10 20:10:39 +0000

    fixed time zone bug
    day and time are corrected if the time exceeds 24 hours after correcting for the time zone

--- pages/utils/allowed_times.tsx
+++ pages/utils/allowed_times.tsx
@@ -2,9 +2,13 @@
 
 export default function allowed_times() {
 	const currentTime = new Date(Date.now()); //utc time
-	const currentDay = currentTime.getDay();
-	const currentHour = currentTime.getHours() + 4; //time utc+4
+	var currentDay = currentTime.getDay();
+	var currentHour = currentTime.getHours() + 4; //time utc+4
 
+	if (currentHour >= 24)
+		currentDay = currentDay + 1
+		currentHour = currentHour - 24
+	
 	// Sunday is 0 and Wednesday is 3 in getDay()
 	// Check if the current day is Sunday or Wednesday after 12 PM (noon)
 	// and before 8 PM the next day (20 hours)
