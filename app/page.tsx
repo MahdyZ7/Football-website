9690de9bd6188f75f65f4a9f82de2b27b5ddd75b
MahdyZ7 <amahdy9@gmail.com>
2025-11-17 00:44:37 +0400

    updated dependances and npm modules

--- app/page.tsx
+++ app/page.tsx
@@ -5,24 +5,25 @@ import Home from '../components/pages/home';
 import FootballApp from '../components/pages/althome';
 
 export default function Index() {
-	const [showHome, setShowHome] = useState(true); // Default to Home for SSR
-	const [isClient, setIsClient] = useState(false);
+	return <Home />;
+	// const [showHome, setShowHome] = useState(true); // Default to Home for SSR
+	// const [isClient, setIsClient] = useState(false);
 
-	useEffect(() => {
-		setIsClient(true);
-		// Only run random selection on client side
-		setShowHome(Math.random() < 0.9);
-	}, []);
+	// useEffect(() => {
+	// 	setIsClient(true);
+	// 	// Only run random selection on client side
+	// 	setShowHome(Math.random() < 0.9);
+	// }, []);
 
-	// During SSR and before client hydration, always show Home
-	if (!isClient) {
-		return <Home />;
-	}
+	// // During SSR and before client hydration, always show Home
+	// if (!isClient) {
+	// 	return <Home />;
+	// }
 
-	// After hydration, show the randomly selected component
-	return (
-		<>
-			{showHome ? <Home /> : <FootballApp />}
-		</>
-	);
+	// // After hydration, show the randomly selected component
+	// return (
+	// 	<>
+	// 		{showHome ? <Home /> : <FootballApp />}
+	// 	</>
+	// );
 }
